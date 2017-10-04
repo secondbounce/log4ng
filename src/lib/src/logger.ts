090bb173dcf8a3bd64e75b8cc84621cbadd4dddf
David Geary <david@secondbounce.com>
2017-10-04 14:34:36 +0100

    Add function overloads to Logger to allow unused parameters to be omitted

--- src/lib/src/logger.ts
+++ src/lib/src/logger.ts
@@ -27,27 +27,45 @@ export class Logger {
     return this.logService.isLevelEnabled(Level.fatal);
   }
 
-  public debug(message: string, exception?: Error): void {
-    this.log(Level.debug, message, exception);
+  public debug(messageOrException: string | Error): void;
+  public debug(message: string, exception: Error): void;
+  public debug(messageOrException: string | Error, exception?: Error): void {
+    this.log(Level.debug, messageOrException, exception);
   }
 
-  public info(message: string, exception?: Error): void {
-    this.log(Level.info, message, exception);
+  public info(messageOrException: string | Error): void;
+  public info(message: string, exception: Error): void;
+  public info(messageOrException: string | Error, exception?: Error): void {
+    this.log(Level.info, messageOrException, exception);
   }
 
-  public warn(message: string, exception?: Error): void {
-    this.log(Level.warn, message, exception);
+  public warn(messageOrException: string | Error): void;
+  public warn(message: string, exception: Error): void;
+  public warn(messageOrException: string | Error, exception?: Error): void {
+    this.log(Level.warn, messageOrException, exception);
   }
 
-  public error(message: string, exception?: Error): void {
-    this.log(Level.error, message, exception);
+  public error(messageOrException: string | Error): void;
+  public error(message: string, exception: Error): void;
+  public error(messageOrException: string | Error, exception?: Error): void {
+    this.log(Level.error, messageOrException, exception);
   }
 
-  public fatal(message: string, exception?: Error): void {
-    this.log(Level.fatal, message, exception);
+  public fatal(messageOrException: string | Error): void;
+  public fatal(message: string, exception: Error): void;
+  public fatal(messageOrException: string | Error, exception?: Error): void {
+    this.log(Level.fatal, messageOrException, exception);
   }
 
-  private log(level: Level, message: string, exception: Error): void {
+  private log(level: Level, messageOrException: string | Error, exception?: Error): void {
+    if (messageOrException instanceof Error) {
+      this.dispatchLoggingEvent(level, '', messageOrException);   /* Use empty string, otherwise 'null' or 'undefined' will be output */
+    } else {
+      this.dispatchLoggingEvent(level, messageOrException, exception);
+    }
+  }
+
+  private dispatchLoggingEvent(level: Level, message: string, exception: Error): void {
     const loggingEvent: LoggingEvent = new LoggingEvent();
     loggingEvent.level = level;
     loggingEvent.loggerName = this.name;
