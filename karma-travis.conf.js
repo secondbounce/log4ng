2fec72e44fa84de044edce0c48e38317fc297897
David Geary <david@secondbounce.com>
2017-10-04 17:32:26 +0100

    Another attempt at a travis-ci configuration to run the unit tests

---
+++ karma-travis.conf.js
@@ -0,0 +1,16 @@
+const karmaConf = require('./karma.conf.js');
+module.exports = function (config) {
+  // Generic Karma Configuration
+  karmaConf(config);
+
+  // Extended configuration for running Karma tests under Travis
+  config.set({
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
+      require('karma-jasmine-html-reporter')
+    ],
+    browsers: ['Chrome', 'Firefox']
+  })
+}
